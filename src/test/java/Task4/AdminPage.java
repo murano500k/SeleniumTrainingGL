091566709ddb850cfaeaddfba3462e2e5bb0c038
Artem <murano500k@gmail.com>
2017-04-04 14:16:19 +0300

    refactoring

--- src/test/java/Task4/AdminPage.java
+++ src/test/java/Task4/AdminPage.java
@@ -22,24 +22,51 @@ public class AdminPage {
     private WebDriver driver;
     private WebDriverWait wait;
 
+    private By parentContainerSelector = By.cssSelector("ul[class=list-vertical]");
     private By childContainerSelector = By.cssSelector("ul[class=docs]");
+    private By parentItemSelector = By.cssSelector("li[id=app-]");
+    private By childItemSelector = By.tagName("li");
+    private By headerSelector = By.cssSelector("h1");
 
     @Before
     public void start() {
 
         ChromeDriverManager.getInstance().setup();
-
         driver = new ChromeDriver();
         wait = new WebDriverWait(driver, 10);
     }
 
+    @Test
+    public void testSelenium() throws IOException {
+        driver.get(BASE_URL+PAGE_URL);
+        driver.findElement(By.name("username")).sendKeys("admin");
+        driver.findElement(By.name("password")).sendKeys("admin");
+        driver.findElement(By.name("login")).click();
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+
+        WebElement parentul = driver.findElement(parentContainerSelector);
+        int parentItemsListSize = parentul.findElements(By.cssSelector("li[id=app-]")).size();
+
+        for (int i = 0; i < parentItemsListSize; i++) {
+            WebElement parentContainer = driver.findElement(parentContainerSelector);
+            List<WebElement> pl = parentContainer.findElements(parentItemSelector);
+            WebElement parentMenuItem = pl.get(i);
+            parentMenuItem.click();
+            wait.withTimeout(5000, TimeUnit.MILLISECONDS);
+            if (checkElementPresent(childContainerSelector)) {
+                clickAllSelectedChildren(childContainerSelector, childItemSelector);
+            } else {
+                System.err.println("no children");
+            }
+
+        }
+    }
+
 
     public void clickAllSelectedChildren(By parentselector, By childselector){
-
         WebElement element = driver.findElement(parentselector);
-        //element.click();
-        int size = element.findElements(childselector).size();
-        for (int i = 0; i < size; i++) {
+        int childItemListSize = element.findElements(childselector).size();
+        for (int i = 0; i < childItemListSize; i++) {
             WebElement h = driver.findElement(parentselector);
             List<WebElement> webElements = h.findElements(childselector);
             WebElement childMenuItem = webElements.get(i);
@@ -51,8 +78,7 @@ public class AdminPage {
 
     }
 
-    public boolean checkeElemetPresence(By selector){
-
+    public boolean checkElementPresent(By selector){
         int size = driver.findElements(selector).size();
         boolean result = size != 0;
         return result;
@@ -61,7 +87,7 @@ public class AdminPage {
 
     private void validateHeader(String header) {
         System.out.println("child menu item (expected): "+ header);
-        WebElement h = driver.findElement(By.cssSelector("h1"));
+        WebElement h = driver.findElement(headerSelector);
         String title = h.getText();
         System.out.println("page header (actual): "+ title);
         if(header!=null && header.contains(title)) System.out.println("Page title correct!");
@@ -70,39 +96,7 @@ public class AdminPage {
     }
 
 
-    @Test
-    public void testSelenium() throws IOException {
-        System.out.println("HELLO");
-        driver.get(BASE_URL+PAGE_URL);
-        driver.findElement(By.name("username")).sendKeys("admin");
-        driver.findElement(By.name("password")).sendKeys("admin");
-        driver.findElement(By.name("login")).click();
-        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
-        WebElement parentul = driver.findElement(By.cssSelector("ul[class=list-vertical]"));
-        //List<WebElement> parentList = parentul.findElements(By.cssSelector("li[id=app-]"));
 
-        int size = parentul.findElements(By.cssSelector("li[id=app-]")).size();
-
-        for (int i = 0; i < size; i++) {
-
-
-            WebElement container = driver.findElement(By.cssSelector("ul[class=list-vertical]"));
-            List<WebElement> pl = container.findElements(By.cssSelector("li[id=app-]"));
-            WebElement parentMenuItem = pl.get(i);
-            parentMenuItem.click();
-            wait.withTimeout(5000, TimeUnit.MILLISECONDS);
-
-            if (checkeElemetPresence(childContainerSelector)) {
-
-                clickAllSelectedChildren(childContainerSelector, By.tagName("li"));
-
-            } else {
-                System.out.println("no children");
-                //clickAllSelectedChildren(By.cssSelector("table[class=dataTable]"), By.tagName("tr[class=row]"));
-            }
-
-        }
-    }
 
     @After
     public void stop() {
